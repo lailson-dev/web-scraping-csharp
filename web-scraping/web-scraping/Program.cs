c5ca40b2666b0e68a4fc45a92fe688cf30872903
Lailson Conceição <lailsoncv@hotmail.com>
2019-03-07 14:18:26 -0300

    Estrutura inicial

---
+++ web-scraping/web-scraping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace web_scraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
